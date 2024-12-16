ba666ae93620e4eb90f7bbf7bf7199751084dd6c
Apurva-Simform <122270609+apurva010@users.noreply.github.com>
2024-12-16 16:10:46 +0530

    🔨 separate out static strings to constant (#26)

---
+++ lib/values/android_strings.dart
@@ -0,0 +1,107 @@
+/// Android strings (Element, Attribute and its value)
+class AndroidStrings {
+  /// `splash_image.png`
+  static const splashImagePng = 'splash_image.png';
+
+  /// `splash_image_12.png`
+  static const splashImage12Png = 'splash_image_12.png';
+
+  /// `colors.xml`
+  static const colorXml = 'colors.xml';
+
+  /// `styles.xml`
+  static const stylesXml = 'styles.xml';
+
+  /// `splash_screen.xml`
+  static const splashScreenXml = 'splash_screen.xml';
+
+  /// `xml`
+  static const xml = 'xml';
+
+  /// `version="1.0" encoding="utf-8"`
+  static const xmlVersion = 'version="1.0" encoding="utf-8"';
+
+  /// Element
+  ///
+  /// `resources`
+  static const resourcesElement = 'resources';
+
+  /// `color`
+  static const colorElement = 'color';
+
+  /// `item`
+  static const itemElement = 'item';
+
+  /// `layer-list`
+  static const layerListElement = 'layer-list';
+
+  /// `style`
+  static const styleElement = 'style';
+
+  /// Attribute
+  ///
+  /// `name`
+  static const nameAttr = 'name';
+
+  /// `splashBgColor`
+  static const nameAttrValue = 'splashBgColor';
+
+  /// `android:windowBackground`
+  static const itemNameAttrValue = 'android:windowBackground';
+
+  /// `@drawable/splash_screen`
+  static const drawableSplashScreen = '@drawable/splash_screen';
+
+  /// `xmlns:android`
+  static const xmlnsAndroidAttr = 'xmlns:android';
+
+  /// `http://schemas.android.com/apk/res/android`
+  static const xmlnsAndroidAttrValue =
+      'http://schemas.android.com/apk/res/android';
+
+  /// `android:drawable`
+  static const androidDrawableAttr = 'android:drawable';
+
+  /// `@color/splashBgColor`
+  static const androidDrawableAttrVal = '@color/splashBgColor';
+
+  /// `bitmap`
+  static const bitmapAttrVal = 'bitmap';
+
+  /// `android:gravity`
+  static const androidGravityAttr = 'android:gravity';
+
+  /// `fill`
+  static const defaultAndroidGravityAttrVal = 'fill';
+
+  /// `android:src`
+  static const androidSrcAttr = 'android:src';
+
+  /// `@mipmap/splash_image`
+  static const androidSrcAttrVal = '@mipmap/splash_image';
+
+  /// `android:tileMode`
+  static const androidTileModeAttr = 'android:tileMode';
+
+  /// `disabled`
+  static const androidTileModeAttrVal = 'disabled';
+
+  /// `LaunchTheme`
+  static const styleNameAttrVal = 'LaunchTheme';
+
+  /// `parent`
+  static const styleParentAttr = 'parent';
+
+  /// `@android:style/Theme.Light.NoTitleBar`
+  static const styleParentAttrVal = '@android:style/Theme.Light.NoTitleBar';
+
+  /// `android:windowSplashScreenBackground`
+  static const windowSplashScreenBG = 'android:windowSplashScreenBackground';
+
+  /// `android:windowSplashScreenAnimatedIcon`
+  static const windowSplashScreenAnimatedIcon =
+      'android:windowSplashScreenAnimatedIcon';
+
+  /// `@mipmap/splash_image12`
+  static const mipmapSplashImage12 = '@mipmap/splash_image12';
+}
