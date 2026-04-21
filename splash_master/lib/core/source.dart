eeb271b1793400e495868c917d5086218b324335
Apurva Simform <apurva.k@simformsolutions.com>
2026-04-21 13:42:28 +0530

    ✨ Refactor project structure

---
+++ splash_master/lib/core/source.dart
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2024 Simform Solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import '../splash_master.dart';
+
+/// Holds the source file
+sealed class Source {
+  void setSource();
+}
+
+final class AssetSource extends Source {
+  /// Provides the asset resource for the splash screen
+  AssetSource(this.path);
+
+  final String path;
+
+  @override
+  Future<void> setSource() async {}
+}
+
+final class DeviceFileSource extends Source {
+  /// Provides the device file for the splash screen
+  DeviceFileSource(this.path) {
+    setSource();
+  }
+
+  final String path;
+
+  late final File file;
+
+  @override
+  Future<void> setSource() async {
+    file = File(path);
+  }
+}
+
+final class NetworkFileSource extends Source {
+  /// Provides the network source for the splash screen
+  NetworkFileSource(this.path) {
+    setSource();
+  }
+
+  final String path;
+
+  Uri? _url;
+
+  Uri? get url => _url;
+
+  @override
+  void setSource() {
+    _url = Uri.tryParse(path);
+    if (_url == null) {
+      throw SplashMasterException(message: 'Unable to parse uri.');
+    }
+  }
+}
+
+final class BytesSource extends Source {
+  /// Provides the source as bytes for the splash screen
+  BytesSource(this.bytes);
+
+  final Uint8List bytes;
+
+  @override
+  void setSource() {}
+}
---
+++ splash_master/lib/values/android_strings.dart
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2024 Simform Solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/// Android strings (Element, Attribute and its value)
+class AndroidStrings {
+  /// `splash_image.png`
+  static const splashImagePng = 'splash_image.png';
+
+  /// `splash_image_dark.png`
+  static const splashImageDarkPng = 'splash_image_dark.png';
+
+  /// `splash_image_12.png`
+  static const splashImage12Png = 'splash_image_12.png';
+
+  /// `android_12_branding_image.png`
+  static const android12BrandingImage = 'android_12_branding_image.png';
+
+  /// `colors.xml`
+  static const colorXml = 'colors.xml';
+
+  /// `styles.xml`
+  static const stylesXml = 'styles.xml';
+
+  /// `splash_screen.xml`
+  static const splashScreenXml = 'splash_screen.xml';
+
+  /// `xml`
+  static const xml = 'xml';
+
+  /// `version="1.0" encoding="utf-8"`
+  static const xmlVersion = 'version="1.0" encoding="utf-8"';
+
+  /// Element
+  ///
+  /// `resources`
+  static const resourcesElement = 'resources';
+
+  /// `color`
+  static const colorElement = 'color';
+
+  /// `item`
+  static const itemElement = 'item';
+
+  /// `layer-list`
+  static const layerListElement = 'layer-list';
+
+  /// `style`
+  static const styleElement = 'style';
+
+  /// Attribute
+  ///
+  /// `name`
+  static const nameAttr = 'name';
+
+  /// `splashBgColor`
+  static const nameAttrValue = 'splashBgColor';
+
+  /// `#FFFFFF`
+  static const defaultLightColor = '#FFFFFF';
+
+  /// `android:windowBackground`
+  static const itemNameAttrValue = 'android:windowBackground';
+
+  /// `@drawable/splash_screen`
+  static const drawableSplashScreen = '@drawable/splash_screen';
+
+  /// `xmlns:android`
+  static const xmlnsAndroidAttr = 'xmlns:android';
+
+  /// `http://schemas.android.com/apk/res/android`
+  static const xmlnsAndroidAttrValue =
+      'http://schemas.android.com/apk/res/android';
+
+  /// `android:drawable`
+  static const androidDrawableAttr = 'android:drawable';
+
+  /// `@color/splashBgColor`
+  static const androidDrawableAttrVal = '@color/splashBgColor';
+
+  /// `bitmap`
+  static const bitmapAttrVal = 'bitmap';
+
+  /// `android:gravity`
+  static const androidGravityAttr = 'android:gravity';
+
+  /// `fill`
+  static const defaultAndroidGravityAttrVal = 'fill';
+
+  /// `android:src`
+  static const androidSrcAttr = 'android:src';
+
+  /// `@drawable/splash_image`
+  static const androidSrcAttrVal = '@drawable/splash_image';
+
+  /// `@drawable/background_image`
+  static const androidBackgroundSrcAttrVal = '@drawable/background_image';
+
+  /// `@drawable/splash_image_dark`
+  static const androidDarkSrcAttrVal = '@drawable/splash_image_dark';
+
+  /// `android:tileMode`
+  static const androidTileModeAttr = 'android:tileMode';
+
+  /// `disabled`
+  static const androidTileModeAttrVal = 'disabled';
+
+  /// `LaunchTheme`
+  static const styleNameAttrVal = 'LaunchTheme';
+
+  /// `parent`
+  static const styleParentAttr = 'parent';
+
+  /// `@android:style/Theme.Light.NoTitleBar`
+  static const styleParentAttrVal = '@android:style/Theme.Light.NoTitleBar';
+
+  /// `android:windowSplashScreenBackground`
+  static const windowSplashScreenBG = 'android:windowSplashScreenBackground';
+
+  /// `android:windowSplashScreenAnimatedIcon`
+  static const windowSplashScreenAnimatedIcon =
+      'android:windowSplashScreenAnimatedIcon';
+
+  /// `android:windowSplashScreenBrandingImage`
+  static const windowSplashScreenBrandingImage =
+      'android:windowSplashScreenBrandingImage';
+
+  /// `@drawable/splash_image12`
+  static const drawableSplashImage12 = '@drawable/splash_image_12';
+
+  /// `splash_image_12_dark.png`
+  static const splashImage12DarkPng = 'splash_image_12_dark.png';
+
+  /// `@drawable/splash_image_12_dark`
+  static const drawableSplashImage12Dark = '@drawable/splash_image_12_dark';
+
+  /// `@drawable/android_12_branding_image`
+  static const drawableAndroid12BrandingImage =
+      '@drawable/android_12_branding_image';
+
+  /// `simform_splash_img.png`
+  static const splashBackgroundImagePng = 'simform_splash_img.png';
+
+  /// `simform_splash_img_dark.png`
+  static const splashBackgroundImageDarkPng = 'simform_splash_img_dark.png';
+
+  /// `@drawable/background_image_dark`
+  static const androidDarkBackgroundSrcAttrVal =
+      '@drawable/background_image_dark';
+
+  /// `android_12_branding_image_dark.png`
+  static const android12BrandingImageDark =
+      'android_12_branding_image_dark.png';
+
+  /// `@drawable/android_12_branding_image_dark`
+  static const drawableAndroid12BrandingImageDark =
+      '@drawable/android_12_branding_image_dark';
+
+  /// `@drawable/splash_screen_dark`
+  static const androidDarkDrawable = '@drawable/splash_screen_dark';
+
+  /// `splash_screen_dark.xml`
+  static const splashScreenDarkXml = 'splash_screen_dark.xml';
+}
